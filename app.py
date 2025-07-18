09ab821e1c4ace4209fa231907dfaee643fc50b6
kynivez <153317283+kynivez@users.noreply.github.com>
2025-07-19 01:13:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -196,4 +196,4 @@ def historical(bank_code):
         return jsonify({"error": "An error occurred while fetching historical data."}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
